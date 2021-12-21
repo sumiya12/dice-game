36277e904eca3b7caddf8b85f059b474541cc232
sumiya <sumiya.udval@gmail.com>
2021-12-21 10:02:21 +0800

    toglogchiin eeljiig solidog bolgono

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 // Тоглогчийн ээлжийг хадагалах хувьсагч, нэгдүгээр тоглогчийг 0 ь хоёрдугаар тоглогчийг 1 гэж тэмдэглэе.
-var activePlayer = 1;
+var activePlayer = 0;
 
 // Тоглогчдын цуглуулсан оноог хадгалах хувьсагч
 var scores = [0, 0];
@@ -18,10 +18,43 @@ document.getElementById("current-0").textContent = "0";
 document.getElementById("current-1").textContent = "0";
 var diceDom = document.querySelector(".dice");
 diceDom.style.display = "none";
-
+//Шоог шидэх эвент листенер
 document.querySelector(".btn-roll").addEventListener("click", function () {
+  //1-6 доторх санамсаргүй нэг тоо гаргаж авна.
   var diceNumber = Math.floor(Math.random() * 6) + 1;
+
+  // Шооны зургийг веб дээр гаргаж ирнэ.
   diceDom.style.display = "block";
+
+  //Буусан санамсаргүй тоонд харгалзах шооны зургийг веб дээр гаргаж ирнэ.
   diceDom.src = "dice-" + diceNumber + ".png";
   //   alert("ШооГ шидлээ : " + diceNumber);
+
+  //Буусан тоо нь 1 ээс ялгаатай бол идэвхтэй тоглогчийн ээлжийн оноог өөрчилнө.
+  if (diceNumber !== 1) {
+    // 1 ээс ялгаатай тоо буулаа. Буусан тоог тоглогчид нэмж өгнө.
+    roundScore = roundScore + diceNumber;
+    document.getElementById("current-" + activePlayer).textContent = roundScore;
+  } else {
+    // 1 буусан тул тоглогчийн ээлжийг энэ хэсэгт сольж өгнө.
+
+    // Энэ тоглогчийн ээлжиндээ цуглуулсан оноог 0 болгоно.
+    roundScore = 0;
+    document.getElementById("current-" + activePlayer).textContent = 0;
+    // Тоглогчийн ээлжиндээ нөгөө тоглогч руу шилжүүлнэ.
+    // Хэрэв идэхвтэй тоглогч нь 0 байвал идэвхтэй тоглогчийг 1 болго.
+    // Үгүй бол идэвхтэй тоглогчийг 0 болго.
+    activePlayer === 0 ? (activePlayer = 1) : (activePlayer = 0);
+
+    // if (activePlayer === 0) {
+    //   activePlayer = 1;
+    // } else {
+    //   activePlayer = 0;
+    // }
+    //  Улаан цэгийг шилжүүлэх код
+    document.querySelector(".player-0-panel").classList.toggle("active");
+    document.querySelector(".player-1-panel").classList.toggle("active");
+    // Шоог түр алга болгоно
+    diceDom.style.display = "none";
+  }
 });
